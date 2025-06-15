a270d31ec78f8bc4fa00bfb8ca1b97b1f90dd12d
Thiago Torres <137134010+ThiagoTorresFerrao@users.noreply.github.com>
2025-06-15 10:44:44 -0300

    Added Menu Hanburguer
    Added hanburguer menu for mobile.
    added script for menu hanburguer.

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: 'smooth' });
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("menu");
+
+  toggle.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+});
