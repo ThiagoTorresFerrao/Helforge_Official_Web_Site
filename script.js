c42fcf1efbd26e3daf69e4caef7c15af5e02284e
Thiago Torres <137134010+ThiagoTorresFerrao@users.noreply.github.com>
2025-06-15 12:06:08 -0300

    Update script and style
    Added movin in the site. added mani css and script files

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ document.addEventListener("DOMContentLoaded", () => {
   const menu = document.getElementById("menu");
 
   toggle.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
+    if (menu.classList.contains("hidden")) {
+      menu.classList.remove("hidden");
+      setTimeout(() => {
+        menu.classList.add("opacity-100", "scale-100");
+        menu.classList.remove("opacity-0", "scale-95");
+      }, 10);
+    } else {
+      menu.classList.remove("opacity-100", "scale-100");
+      menu.classList.add("opacity-0", "scale-95");
+      setTimeout(() => {
+        menu.classList.add("hidden");
+      }, 300); // duração da animação em ms
+    }
   });
 });
